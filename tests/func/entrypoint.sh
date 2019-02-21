d307f64ca68055a15b014135d4f3cd1e44c8477a
Étienne BERSAC <etienne.bersac@dalibo.com>
2019-02-21 10:00:27 +0100

    Execute func test on CI

---
+++ tests/func/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -eux
+#
+# Entrypoint for both GitLab CI and docker-compose.yml
+#
+
+cd $(readlink -m $0/../../..)
+test -f pyproject.toml
+
+export PATH=~/.local/bin:$PATH
+export XDG_CACHE_HOME=${PWD}/.cache/
+
+teardown() {
+    # If not on CI, wait for user interrupt on exit
+    if [ -z "${CI-}" -a $? -gt 0 -a $PPID = 1 ] ; then
+        : Container failed. Debug with:
+        : "    docker exec -it $HOSTNAME /bin/bash"
+        tail -f /dev/null
+    fi
+    sudo chown -R $owner $XDG_CACHE_HOME
+}
+
+owner=$(stat -c %u .)
+runner=$(id -u)
+trap teardown EXIT TERM
+
+if [ $runner -gt 0 -a $owner -ne $runner ] ; then
+    exec sudo -E $0
+fi
+sudo chown -R $runner $XDG_CACHE_HOME
+
+mkdir -p $XDG_CACHE_HOME
+pip --disable-pip-version-check install --user poetry
+poetry install
+poetry run tests/pypsql < dramatiq_pg/schema.sql
+poetry run pytest -x tests/func/
